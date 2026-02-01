283b0c41b36eec487f7a08067d02caa04b73323e
Lachy Schuamcher <lachy@tydewest.com>
2026-02-02 02:34:13 +1100

    0.1.14

--- src/commands.c
+++ src/commands.c
@@ -123,7 +123,7 @@ int fetch_latest_version(VersionInfo *info) {
 void print_help() {
     printf(COLOR_BLUE "Dynaserve CLI Commands:\n" COLOR_RESET);
     printf(COLOR_GREEN "  help" COLOR_RESET "           Show this help message\n");
-    printf(COLOR_GREEN "  --v, --version" COLOR_RESET "  Show installed CLI version\n");
+    printf(COLOR_GREEN "  --version" COLOR_RESET "      Show installed CLI version\n");
     printf(COLOR_GREEN "  --update" COLOR_RESET "       Update to latest version\n");
 }
 
